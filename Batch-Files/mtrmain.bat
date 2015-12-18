28c5b4d7ef80d70077d8fbb6fade69733d9021ee
Grace Ma <grace96ma@gmail.com>
2015-12-18 00:18:47 -0500

    final code

--- Batch-Files/mtrmain.bat
+++ Batch-Files/mtrmain.bat
@@ -14,6 +14,6 @@ asm86 parser.asm m1 ep db
 
 link86 mtrmain.obj, eventq.obj, queue.obj, cs.obj, int.obj, int2.obj, timer1.obj TO temp.lnk
 link86 serial.obj, converts.obj, motors.obj, parinit.obj, trigtbl.obj, parser.obj TO orary.lnk 
-link86 temp.lnk, orary.lnk TO mtrmain.lnk
+link86 temp.lnk, orary.lnk TO mtrmain.lnk 
 loc86 mtrmain.lnk TO mtrmain NOIC AD(SM(CODE(4000h), data(400h), stack(7000h)))
 
